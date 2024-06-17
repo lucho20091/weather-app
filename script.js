9935a0c3e372253258ec7804a867eb839807b150
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-16 21:20:20 -0500

    getting the javascript

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const key = "f27ce85670a99a4166190adb76e403ba"
 const goFS = document.getElementById("goFS");
 const fullScreenElem = document.querySelector("#full-screen")
+const coords = JSON.parse(localStorage.getItem("coords"));
+
+console.log(coords.lat)
 
 
 // fetching the data
 async function getWeather(){
     try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=bogota&appid=${key}`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${key}`)
     const data = await response.json()
     console.log(data)
     } catch(e){
@@ -15,6 +18,19 @@ async function getWeather(){
 }
 
 getWeather()
+console.log(coords);
+
+// get the location
+function getLocation(){
+   navigator.geolocation.getCurrentPosition((position) => {
+      localStorage.setItem("coords", JSON.stringify({
+         lat: position.coords.latitude.toFixed(2),
+         lon: position.coords.longitude.toFixed(2)
+      }))
+   })
+}
+
+getLocation()
 
 // handling the full screen btn
 goFS.addEventListener("click", function() {
