e6225f56602c44cf007dc43c2ac93c95b3cbf999
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-06-17 11:57:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,30 +5,48 @@ const coords = JSON.parse(localStorage.getItem("coords"));
 const city = document.getElementById("city");
 const temp = document.getElementById("temp");
 const weather = document.getElementById("weather");
-const tempMinMax = document.getElementById("temp-max-min");
-
+const date = document.getElementById("temp-max-min");
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&units=metric&appid=${key}`
+const url2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${coords.lat}&lon=${coords.lon}&units=metric&appid=${key}`
+const bottomMid = document.querySelector(".bottom-mid")
 // fetching the data
 async function getWeather(){
     try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&units=metric&appid=${key}`)
+    const response = await fetch(url2)
     const data = await response.json()
     console.log(data)
-    console.log(data.name, data.sys.country)
     renderHTML(data)
+    renderForecastHTML(data)
     } catch(e){
         console.log(e)
     }
 }
 
 getWeather()
-
+// render html main
 function renderHTML(data){
-   city.textContent = data.name
-   temp.textContent = `${Math.floor(data.main.temp)}°`
-   weather.textContent = data.weather[0].description
-   tempMinMax.textContent = `H: ${Math.floor(data.main.temp_max)}° L: ${Math.floor(data.main.temp_min)}°`
+   city.textContent = data.city.name
+   temp.textContent = `${Math.floor(data.list[0].main.temp)}°`
+   weather.textContent = data.list[0].weather[0].description
+   date.textContent = `${data.list[0].dt_txt.slice(5,7)}/${data.list[0].dt_txt.slice(8,10)}`
+   console.log(data.list[0].dt_txt.slice(11,16))
 }
 
+// render html bottom
+function renderForecastHTML(data){
+   console.log(data)
+   let html = ''
+   for (let i = 0; i < 5; i++){
+      html += `
+      <div class="bottom-mid-pill">
+            <p>${data.list[i].dt_txt.slice(11,16)}</p>
+            <p>${data.list[i].dt_txt.slice(5,7)}/${data.list[i].dt_txt.slice(8,10)}</p>
+            <p class="bigger-p">${Math.floor(data.list[i].main.temp)}°</p>
+      </div>`
+   }
+   console.log(html)
+   bottomMid.innerHTML = html
+}
 
 // get the location
 function getLocation(){
